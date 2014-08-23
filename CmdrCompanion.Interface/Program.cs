f1fda0fbda35165007cd0038b360cdf517d7705d
Shtong <shtong@mm.st>
2014-08-23 10:32:40 +0200

    Updated the build process to embed external DLLs inside the main assembly

---
+++ CmdrCompanion.Interface/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CmdrCompanion.Interface
+{
+    public static class Program
+    {
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+
+            App app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+        static System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            string asmName = String.Format("CmdrCompanion.Interface.ExternalDlls.{0}.dll", new AssemblyName(args.Name).Name);
+            using(Stream s = Assembly.GetExecutingAssembly().GetManifestResourceStream(asmName))
+            {
+                byte[] asmBytes = new byte[s.Length];
+                s.Read(asmBytes, 0, asmBytes.Length);
+                return Assembly.Load(asmBytes);
+            }
+        }
+    }
+}
