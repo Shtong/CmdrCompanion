0eeaefd77bec0c6bbdd1c7c6c836ce1df1a19c5b
Shtong <shtong@mm.st>
2014-08-24 10:49:56 +0200

    First implementation of the updater. !UNTESTED! Will test after the UI branch is merged

--- CmdrCompanion.Interface/ViewModel/MainViewModel.cs
+++ CmdrCompanion.Interface/ViewModel/MainViewModel.cs
@@ -2,10 +2,14 @@ using CmdrCompanion.Core;
 using CmdrCompanion.Interface.Properties;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Threading;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace CmdrCompanion.Interface.ViewModel
 {
@@ -38,8 +42,11 @@ namespace CmdrCompanion.Interface.ViewModel
             ////{
             ////    // Code runs "for real"
             ////}
+
+            
         }
 
+        private DispatcherTimer _updatePollTimer;
 
         private void InitializeApp()
         {
@@ -73,6 +80,40 @@ namespace CmdrCompanion.Interface.ViewModel
                 }
             }
 
+        }
+
+        private void InitializeUpdates()
+        {
+            // In 10 seconds, see if there are temp files still there
+            // We wait to give some time for the updater process to exit and unlock the files
+            Timer cleanTimer = new Timer(CleanupWorker, null, 10000, Timeout.Infinite);
+
+            // Start checking for updates
+            Updater.Check();
+
+            // Every 2 seconds see if we have a result
+            _updatePollTimer = new DispatcherTimer(new TimeSpan(0, 2, 0), DispatcherPriority.Background, PollUpdater, DispatcherHelper.UIDispatcher);
+            _updatePollTimer.Start();
+        }
+
+        private void CleanupWorker(object state)
+        {
+            Updater.Cleanup();
+        }
+
+        private void PollUpdater(object sender, EventArgs e)
+        {
+            if(Updater.ShouldUpdate.HasValue)
+            {
+                _updatePollTimer.Stop();
+
+                if(Updater.ShouldUpdate.Value)
+                {
+                    Updater.StartUpdating();
+                    // Bye !
+                    Application.Current.Shutdown();
+                }
+            }
         }
     }
 }
---
+++ CmdrCompanion.Updater/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace CmdrCompanion.Updater
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string BASE_URL = "http://cmdr-companion.shtong.mm.st/";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Uri baseUri = new Uri(BASE_URL);
+            WebClient client = new WebClient();
+            string tempFolder = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+            string archivePath = Path.Combine(tempFolder, "update.zip");
+            string softPath = Process.GetCurrentProcess().StartInfo.Arguments;
+
+            if(!Directory.Exists(softPath))
+            {
+                info.Text = "Could not find the software folder :(";
+                Trace.WriteLine("Could not find folder " + softPath);
+                return;
+            }
+
+            try
+            {
+                // Download the version number
+                string sVersion = await client.DownloadStringTaskAsync(new Uri(baseUri, "version.txt"));
+                Version version = null;
+                if (!Version.TryParse(sVersion, out version))
+                {
+                    info.Text = "Invalid remote version :(";
+                    Trace.WriteLine("Downloaded version code: " + sVersion);
+                    return;
+                }
+
+                // Download the archive
+                await client.DownloadFileTaskAsync(new Uri(baseUri, version.ToString() + ".zip"), archivePath);
+
+                // Wait for the application to close
+                info.Text = "Waiting for the application to close...";
+                await Task.Run(() =>
+                {
+                    EventWaitHandle handle = null;
+                    if(EventWaitHandle.TryOpenExisting("CmdrCompanion#running", out handle))
+                        handle.WaitOne();
+                });
+
+                info.Text = "Installing...";
+                // Remove the previous version
+                foreach (string fPath in Directory.EnumerateFiles(softPath))
+                    File.Delete(fPath);
+                foreach (string dPath in Directory.EnumerateDirectories(softPath))
+                    Directory.Delete(dPath, true);
+
+                // Extract the new version
+                ZipFile.ExtractToDirectory(archivePath, softPath);
+
+                // And launch the newly installed version !
+                Process.Start(Path.Combine(softPath, "CmdrCompanion.exe"));
+
+                // Aaand it's done
+                Application.Current.Shutdown();
+            }
+            catch(WebException ex)
+            {
+                info.Text = "Could not download the update :(";
+                Trace.WriteLine("Download error: " + ex.Message);
+                return;
+            }
+        }
+
+    }
+}
