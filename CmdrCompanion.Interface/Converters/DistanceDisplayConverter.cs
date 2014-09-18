555e56c5ffdb4b043ae4bc476fd84b9f3a1e0ee3
Shtong <shtong@mm.st>
2014-09-18 19:49:12 +0200

    WHY DID I REMOVE THESE FILES DAMMIT

---
+++ CmdrCompanion.Interface/Converters/DistanceDisplayConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace CmdrCompanion.Interface.Converters
+{
+    public class DistanceDisplayConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is float?)
+            {
+                float? fValue = (float?)value;
+                if (!fValue.HasValue)
+                    return "<unknown>";
+                else
+                    return fValue.Value.ToString(NumberFormatInfo.InvariantInfo) + "Ly";
+            }
+            else if (value is float)
+            {
+                return ((float)value).ToString(NumberFormatInfo.InvariantInfo) + "Ly";
+            }
+            else
+                return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
